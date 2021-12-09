f4c1b8e47bc22500fe4d9dec0a5199df08c57d0e
Belen Pruvost <belen.pruvost@sonarsource.com>
2021-12-09 10:21:09 +0100

    More issues

--- src/main/java/com/sonarsource/SimpleClass.java
+++ src/main/java/com/sonarsource/SimpleClass.java
@@ -1,5 +1,9 @@
 package com.sonarsource;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class SimpleClass {
 
   public boolean authenticate(javax.servlet.http.HttpServletRequest request, java.sql.Connection connection) throws SQLException {
@@ -11,49 +15,28 @@ public class SimpleClass {
 
     String query = "SELECT * FROM users WHERE user = '" + user + "' AND pass = '" + pass + "'"; // Unsafe
 
-    // If the special value "foo' OR 1=1 --" is passed as either the user or pass, authentication is bypassed
-    // Indeed, if it is passed as a user, the query becomes:
-    // SELECT * FROM users WHERE user = 'foo' OR 1=1 --' AND pass = '...'
-    // As '--' is the comment till end of line syntax in SQL, this is equivalent to:
-    // SELECT * FROM users WHERE user = 'foo' OR 1=1
-    // which is equivalent to:
-    // SELECT * FROM users WHERE 1=1
-    // which is equivalent to:
-    // SELECT * FROM users
-
     java.sql.Statement statement = connection.createStatement();
     java.sql.ResultSet resultSet = statement.executeQuery(query); // Noncompliant
     return resultSet.next();
   }
 
-  public User getUser(Connection con, String user) throws SQLException {
-
-    Statement stmt1 = null;
-    Statement stmt2 = null;
-    PreparedStatement pstmt;
-    try {
-      stmt1 = con.createStatement();
-      ResultSet rs1 = stmt1.executeQuery("GETDATE()"); // No issue; hardcoded query
-
-      stmt2 = con.createStatement();
-      ResultSet rs2 = stmt2.executeQuery("select FNAME, LNAME, SSN " +
-        "from USERS where UNAME=" + user);  // Sensitive
-
-      pstmt = con.prepareStatement("select FNAME, LNAME, SSN " +
-        "from USERS where UNAME=" + user);  // Sensitive
-      ResultSet rs3 = pstmt.executeQuery();
-
-      //...
-    }
-
-    public User getUserHibernate(org.hibernate.Session session, String data) {
-
-      org.hibernate.Query query = session.createQuery(
-        "FROM students where fname = " + data);  // Sensitive
-      // ...
-    }
 
     int foo(int a) {
+
+      java.util.regex.Pattern.compile("(a+)+").matcher(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaa!").matches(); // Sensitive
+
+      java.util.regex.Pattern.compile("(h|h|ih(((i|a|c|c|a|i|i|j|b|a|i|b|a|a|j))+h)ahbfhba|c|i)*").matcher(
+        "hchcchicihcchciiicichhcichcihcchiihichiciiiihhcchi"+
+          "cchhcihchcihiihciichhccciccichcichiihcchcihhicchcciicchcccihiiihhihihihi"+
+          "chicihhcciccchihhhcchichchciihiicihciihcccciciccicciiiiiiiiicihhhiiiihchccch"+
+          "chhhhiiihchihcccchhhiiiiiiiicicichicihcciciihichhhhchihciiihhiccccccciciihh"+
+          "ichiccchhicchicihihccichicciihcichccihhiciccccccccichhhhihihhcchchihih"+
+          "iihhihihihicichihiiiihhhhihhhchhichiicihhiiiiihchccccchichci").matches(); // Sensitive
+      
       int b = 12;
       if (a == 1) {
         return b;
