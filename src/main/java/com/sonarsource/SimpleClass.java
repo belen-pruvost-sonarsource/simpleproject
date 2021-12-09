c631d01dd33028f3b0c0a1d6156b4e0420179eef
Belen Pruvost <belen.pruvost@sonarsource.com>
2021-12-09 10:30:49 +0100

    New version

--- src/main/java/com/sonarsource/SimpleClass.java
+++ src/main/java/com/sonarsource/SimpleClass.java
@@ -36,6 +36,21 @@ public class SimpleClass {
           "chhhhiiihchihcccchhhiiiiiiiicicichicihcciciihichhhhchihciiihhiccccccciciihh"+
           "ichiccchhicchicihihccichicciihcichccihhiciccccccccichhhhihihhcchchihih"+
           "iihhihihihicichihiiiihhhhihhhchhichiicihhiiiiihchccccchichci").matches(); // Sensitive
+
+
+      java.util.regex.Pattern.compile("(a+)+").matcher(
+        "aaaaaaaaaaaaabbbaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"+
+          "aaaaaaaaaaaaaaa!").matches(); // Sensitive
+
+      java.util.regex.Pattern.compile("(h|h|ih(((i|a|c|c|a|i|i|j|b|a|i|b|a|a|j))+h)ahbfhba|c|i)*").matcher(
+        "hchcchicihcchciiicichhcichcihcchiihichiciiiihhcchi"+
+          "cchhcihchcihiihciichhccciccichcichiihcchcihhicchcciicchcccihiiihhihihihi"+
+          "chicihhcciccchihhhcchichzcsdgagasdfchciihiicihciihcccciciccicciiiiiiiiicihhhiiiihchccch"+
+          "chhhhiiihchihcccchhhiiiiiiiicicichicihcciciihichhhhchihciiihhiccccccciciihh"+
+          "ichiccchhicchicihihccichicciihcichccihhiciccccccccichhhhihihhcchchihih"+
+          "iihhihihihicichihiiiihhhhihhhchhichiicihhiiiiihchccccchichci").matches(); // Sensitive
       
       int b = 12;
       if (a == 1) {
