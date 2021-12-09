a05fb98649ad89e383a263d12e67521eefafd92e
Belen Pruvost <belen.pruvost@sonarsource.com>
2021-12-09 09:54:49 +0100

    More hotspots

--- src/main/java/com/sonarsource/SimpleClass.java
+++ src/main/java/com/sonarsource/SimpleClass.java
@@ -3,6 +3,9 @@ package com.sonarsource;
 public class SimpleClass {
 
   public boolean authenticate(javax.servlet.http.HttpServletRequest request, java.sql.Connection connection) throws SQLException {
+
+    Connection conn = DriverManager.getConnection("jdbc:derby:memory:myDB;create=true", "login", "");
+
     String user = request.getParameter("user");
     String pass = request.getParameter("pass");
 
@@ -22,4 +25,31 @@ public class SimpleClass {
     java.sql.ResultSet resultSet = statement.executeQuery(query); // Noncompliant
     return resultSet.next();
   }
+
+  public User getUser(Connection con, String user) throws SQLException {
+
+    Statement stmt1 = null;
+    Statement stmt2 = null;
+    PreparedStatement pstmt;
+    try {
+      stmt1 = con.createStatement();
+      ResultSet rs1 = stmt1.executeQuery("GETDATE()"); // No issue; hardcoded query
+
+      stmt2 = con.createStatement();
+      ResultSet rs2 = stmt2.executeQuery("select FNAME, LNAME, SSN " +
+        "from USERS where UNAME=" + user);  // Sensitive
+
+      pstmt = con.prepareStatement("select FNAME, LNAME, SSN " +
+        "from USERS where UNAME=" + user);  // Sensitive
+      ResultSet rs3 = pstmt.executeQuery();
+
+      //...
+    }
+
+    public User getUserHibernate(org.hibernate.Session session, String data) {
+
+      org.hibernate.Query query = session.createQuery(
+        "FROM students where fname = " + data);  // Sensitive
+      // ...
+    }
 }
